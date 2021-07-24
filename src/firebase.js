ecde326ed1af0f8086604ef5b7d3119bd0a7d96d
Arun Kumar <ak.arunkarnan@gmail.com>
2021-07-24 07:12:24 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTHx47s2QRRI9EXlnaD8A1HLhBseq7Txs",
+    authDomain: "clone-c3d5a.firebaseapp.com",
+    projectId: "clone-c3d5a",
+    storageBucket: "clone-c3d5a.appspot.com",
+    messagingSenderId: "263995622239",
+    appId: "1:263995622239:web:b0b9d79ee854c8c1572cea",
+    measurementId: "G-RV1892ZHNT"
+  };
